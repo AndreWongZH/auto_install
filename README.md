6fde430c889df043edc3c98c7ee01e1a9a5528b6
andre wong <wongandre98@gmail.com>
2024-05-18 10:56:44 +0800

    add a readme

---
+++ README.md
@@ -0,0 +1,5 @@
+installation currently supports brew and apt install
+
+- auto_install.sh installs zsh and will then call zsh_upgrade.sh script
+- zsh_upgrade.sh will install oh my zsh, auto suggestions, zsh syntax highlighting, powerlever10k, etc...
+- lazyvim_install.sh currently only installs nvim
--- zsh_upgrade.sh
+++ zsh_upgrade.sh
@@ -132,24 +132,5 @@ fi
 
 echo
 
-if ! command -v nvim &>/dev/null; then
-  echo "neovim not installed, installing ..."
-
-  if command -v apt &>/dev/null; then
-    sudo apt install -y neovim
-  elif command -v brew &>/dev/null; then
-    brew install neovim
-  else
-    echo "unable to install neovim, please install it manually"
-    exit 1
-  fi
-fi
-if [ $? -eq 0 ]; then
-  echo "neovim installed"
-fi
-
-
-echo
-
 source ~/.zshrc
 
